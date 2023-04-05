d4737c5f5469eb7a799c1a1a8b853eebc05668bb
Sukesh Y <sukesh.yerrabommanahalli@pennywisesolutions.com>
2023-04-05 10:39:29 +0530

    Included solr config for web; Correct typo in wpp.targets file; Removed unnecessary config setting; Created a local publish profile;

--- src/Foundation/code/Stratum.Foundation.Common/CommonConfigurations.cs
+++ src/Foundation/code/Stratum.Foundation.Common/CommonConfigurations.cs
@@ -8,7 +8,6 @@ namespace Stratum.Foundation.Common
     /// </summary>
     public class CommonConfigurations
     {
-        public static string AppName => Settings.GetSetting("AppName");
         public static string PassPhrase = Settings.GetSetting("PassPhrase");
         public static string Domain = Settings.GetSetting("Domain");
         public static string PageNotFoundPageUrl = Settings.GetSetting("PageNotFoundPageUrl");
